9699a4e3ecd8aa7c6b60998c9d7f70b23c105daf
Alex Karlo <aleksandrakarlo2@gmail.com>
2023-01-18 22:48:15 +0100

    day3

--- week1/.eslintrc.js
+++ week1/.eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parser": "@babel-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: 'babel-eslint',
+  },
+  rules: {
+  },
+};
