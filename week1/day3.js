ee3e7343a3a91342115f1e634e3bd55e3f3fd272
Alex Karlo <aleksandrakarlo2@gmail.com>
2023-01-18 22:48:49 +0100

    day3

---
+++ week1/day3.js
@@ -0,0 +1,48 @@
+/* eslint-disable linebreak-style */
+const { dns } = require('dns');
+
+// dns.lookup('https://www.google.com/', 'utf8', (error, data) => {});
+
+//     if(error===null){
+//         console.log(data);
+//     } else {
+//         console.log('Oh no !', error);
+//     }
+// });
+
+// const { readfile } = require('fs'); // const {readfile} = require('fs').promises
+// const { promisify } = require('util');
+
+// //const readFilePromised = promisify(readfile);
+
+// (async() => {
+//     try{
+//     const data = await promisify(readfile)('./index123.js', 'utf8');
+// //const data = await readFilePromised('./index123.js', 'utf8');
+//     console.log(data);
+//     } catch(err) {
+//         console.log('On no', err);
+//     }
+// })();
+
+// sync version
+// readfile('./index123.js', 'utf8')
+//     .then(data => {
+//     console.log(data);
+// })
+// .catch(error =>{
+//     console.log('Oh no', error);
+// });
+
+// without promisify
+// readfile('./index123.js', 'utf8', (error, data) => {
+//     if(error===null){
+//         console.log(data);
+//     } else {
+//         console.log('Oh no !', error);
+//     }
+// });
+
+console.log('Program has finished!');
+
+// SIDE TASK WITH DNS
