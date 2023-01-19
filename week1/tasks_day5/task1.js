a1a273eaef4cbb80916e0495029809546cf8387f
Alex Karlo <aleksandrakarlo2@gmail.com>
2023-01-20 00:18:46 +0100

    day5

---
+++ week1/tasks_day5/task1.js
@@ -0,0 +1,14 @@
+/* 1. Write do file ./data/input1.json such data::
+    [1,2,7,20,56,22]
+    Write a program to open this file
+    then sum all the numbers and write this sum to file ./data/sum/txt
+    
+*/
+
+//reading file
+//when ends
+//json to table (parse)
+//summing
+//writing file
+//when ends
+//info about result/ error
---
+++ week1/tasks_day5/task2.js
@@ -0,0 +1,15 @@
+/* 2 .Program which will show the inside of all files in catalogue PROJECTS 
+    +Use option to list files in folder. Then use loop to show them.
+    +the use stat and .isDirectory() to go recursively to other cataloguesand 
+
+
+*/
+
+async function (path) {
+
+    if(...){
+        await showFilesInDirectory(...)
+    }
+}
+
+showFilesInDirectory('.');
