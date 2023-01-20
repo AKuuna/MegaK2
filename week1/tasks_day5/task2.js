64fb6c4ba4e29d418a0991e537f74d58289e665f
Aleksandra.Karlo <aleksandrakarlo@beefee.co.uk>
2023-01-20 13:50:32 +0100

    day5 task2 done

--- week1/tasks_day5/task2.js
+++ week1/tasks_day5/task2.js
@@ -1,15 +1,34 @@
 /* 2 .Program which will show the inside of all files in catalogue PROJECTS 
     +Use option to list files in folder. Then use loop to show them.
     +the use stat and .isDirectory() to go recursively to other cataloguesand 
-
-
 */
 
-async function (path) {
+const {readdir, stat, readFile } = require ('fs').promises;
 
-    if(...){
-        await showFilesInDirectory(...)
+const PATH = './projects/';
+
+async function showFilesInDirectory(PATH) {
+    try {
+    const fileList = await readdir(PATH);
+    // console.log(fileList);
+
+    for (let element of fileList) {
+        const elementPath = `${PATH}/${element}`;
+        const elementsStats = await stat(elementPath);
+        //console.log('Data shared in:', elementPath, elementsStats.isDirectory());
+        
+        if(elementsStats.isFile()){
+            const elementInsides = await readFile(elementPath, 'utf8');
+            console.log('File content: ', elementPath, elementInsides);
+
+
+        } else if(elementsStats.isDirectory()){
+            await showFilesInDirectory(elementPath);
+        }
     }
+    } catch (err) {
+        console.log('Something went wrong', err);
+}
 }
 
-showFilesInDirectory('.');
+showFilesInDirectory(PATH);
